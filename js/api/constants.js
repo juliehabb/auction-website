5fb5382d1fde99c8cfc9109e99bebd4c877e172e
juliehabb <julie.habbestad@hotmail.com>
2024-11-28 19:23:20 +0100

    constants

---
+++ js/api/constants.js
@@ -0,0 +1,14 @@
+
+export const API_KEY = "62d12d31-acd7-41e3-b732-81a5cc7aa742";
+
+export const API_BASE = "https://v2.api.noroff.dev";
+
+export const API_AUTH = `${API_BASE}/auth`;
+
+export const API_AUTH_LOGIN = `${API_AUTH}/login`;
+
+export const API_AUTH_REGISTER = `${API_AUTH}/register`;
+
+export const API_AUTH_KEY = `${API_AUTH}/create-api-key`;
+
+export const API_SOCIAL = `${API_BASE}/social`;

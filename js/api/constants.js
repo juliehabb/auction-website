ddbd67e3b928777e9e9fb36161db8d950cc7271b
juliehabb <julie.habbestad@hotmail.com>
2024-12-14 20:26:53 +0100

    new paths

--- js/api/constants.js
+++ js/api/constants.js
@@ -1,5 +1,5 @@
 
-export const API_KEY = "62d12d31-acd7-41e3-b732-81a5cc7aa742";
+export const API_KEY = "31623f63-2c87-4cff-be6b-df560729269d";
 
 export const API_BASE = "https://v2.api.noroff.dev";
 
@@ -11,4 +11,9 @@ export const API_AUTH_REGISTER = `${API_AUTH}/register`;
 
 export const API_AUTH_KEY = `${API_AUTH}/create-api-key`;
 
-export const API_SOCIAL = `${API_BASE}/social`;
+export const API_AUCTION = `${API_BASE}/auction`;
+
+export const API_AUCTION_LISTINGS = `${API_AUCTION}/listings`;
+
+export const API_AUCTION_PROFILES = `${API_AUCTION}/profiles`;
+
